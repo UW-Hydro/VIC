97090e094aa24f71d2487ca76510c6b41ddd877b
vicadmin <vicadmin@hydro.washington.edu>
1998-11-20 23:48:44 +0000

    removed yearly_epot removed multiplication by SEC_PER_DAY since it was being done in estimate_dew_point

--- priestley.c
+++ priestley.c
@@ -4,26 +4,35 @@
 
 static char vcid[] = "$Id$";
 
-double priestley(double tair, double rad)
+double priestley(double tair, 
+		 double rad)
 /**********************************************************************
 	priestley	Dag Lohmann		January 1996
 
   This routine computes potential evaporation using the Priestly-Taylor
   method.
 
-  REFERENCE:	Handbook of Hydrology pg 4.16-17
+  REFERENCE:	Kimball, J. S., et. al., "An Improved method for estimating
+                surface humidity from daily minimum temperature",
+		Agriculture and Froest Meteorology, 85(1997) 87-98.
 
-  UNITS: 	slope	kPa/C
+  double tair    C       Current air temperature
+  double rad     kJ/day/m^2   Net radiation at the surface
+
+  returns potential evapotranspiration in kg-m^2/s
 
 **********************************************************************/
 {
+
   double slope, pot;
 
-  slope = svp_slope(tair); 
-  /* calculate Priestley-Taylor potential evaporation, the radiation 
-     returned by fltrad is in kJ/day/m^2 */
+  slope = svp_slope(tair); /* gradient in kPa/C */
     
-  pot = ALPHA_PT * slope/(slope + GAMMA_PT) * 0.9 * rad/ LV_PT *
-          SEC_PER_DAY;
+  pot = ALPHA_PT * slope/(slope + GAMMA_PT) * 0.9 * rad / LV_PT;
+  /** The 0.9 is because ground heat flux (G) is assumed to be 10% of the
+      net radiation (Rn), so the radiation term (Rn - G) can be 
+      approximated as (0.9 * Rn) **/
+
   return pot;
+
 }

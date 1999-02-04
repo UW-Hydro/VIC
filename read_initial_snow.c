accded11866ebb4abebfc12b62d73d08488c1818
vicadmin <vicadmin@hydro.washington.edu>
1999-02-04 23:55:38 +0000

    Initial revision

---
+++ read_initial_snow.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vicNl.h>
+#include <string.h>
+
+static char vcid[] = "$Id$";
+
+snow_data_struct read_initial_snow(FILE *initsnow,
+				   int cellnum,
+				   int veg,
+				   int band)
+/******************************************************************************
+  read_initial_snow	Keith Cherkauer	      January 11, 1999
+
+  This routine reads parameters for an initial snowpack - if present.
+
+  Parameters Read from File:
+  TYPE   NAME                    UNITS   DESCRIPTION
+  int    cellnum                 N/A     cell number for current data
+  char   snow                    N/A     1 = snow is present, 0 = no snow present
+  int    last_snow               (dt)    number of time steps since last snowfall
+  double swq                     mm      snow water equivalent
+  double surf_temp               C       surface temperature of snowpack
+  double density                 kg.m^3  density of snowpack
+
+  Parameters Computed from those in the File:
+  TYPE   NAME                    UNITS   DESCRIPTION
+  double coverage                fract   fraction of grid cell covered in snow
+
+  Modifications:
+
+**********************************************************************/
+{
+  extern option_struct options;
+  extern debug_struct debug;
+
+  char             ErrStr[MAXSTRING];
+  char             tmpstr[MAXSTRING];
+  int              index, tmpveg, tmpband;
+  snow_data_struct temp; 
+
+  rewind(initsnow);
+
+  fscanf(initsnow, "%s", tmpstr);
+  if(tmpstr[0] != '#') {
+    index = atoi(tmpstr);
+    fscanf(initsnow, "%i %i", &tmpveg, &tmpband);
+  }
+  else index = tmpveg = tmpband = -999;
+  while(index != cellnum && veg != tmpveg && band != tmpband) {
+    fgets(tmpstr,MAXSTRING,initsnow);
+    fscanf(initsnow, "%s", tmpstr);
+    if(tmpstr[0] != '#') {
+      index = atoi(tmpstr);
+      fscanf(initsnow, "%i %i", &tmpveg, &tmpband);
+    }
+    else index = tmpveg = tmpband = -999;
+  }
+
+  if(!feof(initsnow)) {
+    fscanf(initsnow, "%s",  &tmpstr);
+    if(tmpstr[0]=='1') temp.snow = TRUE;
+    else temp.snow = FALSE;
+    fscanf(initsnow, "%i",  &temp.last_snow);
+    fscanf(initsnow, "%lf", &temp.swq);
+    fscanf(initsnow, "%lf", &temp.surf_temp);
+    fscanf(initsnow, "%lf", &temp.density);
+
+    if(temp.swq < MAX_FULL_COVERAGE_SWQ)
+      temp.coverage = 1. / MAX_FULL_COVERAGE_SWQ * temp.swq;
+    else if(temp.swq > 0.) temp.coverage = 1.0;
+    else temp.coverage = 0.0;
+  }
+  else {
+    /** Assume no snow cover **/
+    fprintf(stderr,"No snow cover defined for cell number %i.\nModel assuming no snow cover.\n",cellnum);
+    temp.snow      = 0;
+    temp.last_snow = 0;
+    temp.swq       = 0.0;
+    temp.surf_temp = 0.0;
+    temp.density   = 0.0;
+    temp.coverage  = 0.0;
+  }
+    
+  return temp;
+
+} 
---
+++ read_initial_soil_thermal.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vicNl.h>
+#include <string.h>
+
+static char vcid[] = "$Id$";
+
+void read_initial_soil_thermal(FILE   *initsoil,
+			       int     cellnum,
+			       int     veg,
+			       int     band,
+			       int     Nnodes,
+			       double  dp,
+			       double *fdepth,
+			       double *moist,
+			       double *ice,
+			       double *dz,
+			       double *T)
+/******************************************************************************
+  read_initial_snow_thermal 	Keith Cherkauer	      January 12, 1999
+
+  This routine reads parameters for initial soil thermal conditions - if defined
+
+  Parameters Read from File:
+  TYPE    NAME                    UNITS   DESCRIPTION
+  int     cellnum                 N/A     number of current cell
+  int     veg                     N/A     vegetation number within cell
+  int     band                    N/A     snow band number within cell
+  double *fdepth                  m       depth of freezing and thawing fronts
+  double *moist                   mm/mm   moisture content of each soil layer
+  double *ice                     mm/mm   ice content of each soil layer
+  double *thermdepths             m       depth from surface of each thermal soln node
+  double *T                       C       temperature at each thermal soln node depth
+
+  Parameters Computed from those in the File:
+  TYPE    NAME                    UNITS   DESCRIPTION
+  double *dz                      m       thickness of soil layer between thermal nodes
+
+  Modifications:
+
+**********************************************************************/
+{
+  extern option_struct options;
+  extern debug_struct debug;
+
+  char             ErrStr[MAXSTRING];
+  char             tmpstr[MAXSTRING];
+  int              i, tmpint, tmpveg, tmpband, index, tmpnodes;
+  double           *thermdepths;
+
+  rewind(initsoil);
+
+  fscanf(initsoil, "%s", tmpstr);
+  if(tmpstr[0] != '#') {
+    index = atoi(tmpstr);
+    fscanf(initsoil, "%i %i", &tmpveg, &tmpband);
+  }
+  else index = tmpveg = tmpband = -999;
+  while(index != cellnum && veg != tmpveg && band != tmpband) {
+    fgets(tmpstr,MAXSTRING,initsoil);
+    fscanf(initsoil, "%s", tmpstr);
+    if(tmpstr[0] != '#') {
+      index = atoi(tmpstr);
+      fscanf(initsoil, "%i %i", &tmpveg, &tmpband);
+    }
+    else index = tmpveg = tmpband = -999;
+  }
+
+  if(!feof(initsoil) && options.FROZEN_SOIL) {
+
+    /** Read Soil Thermal Initialization File for Activated Frozen Soil Model **/
+
+    thermdepths = (double*)calloc(Nnodes,sizeof(double));
+  
+    fscanf(initsoil, "%i",  &tmpnodes);
+    if(Nnodes!=tmpnodes) {
+      sprintf(ErrStr,"Number of nodes defined in soil thermal initialization file (%i), not equal to number of nodes defined in model (%i).",tmpnodes,Nnodes);
+    }
+    for(i=0;i<2;i++)       fscanf(initsoil, "%lf",     &fdepth[i]);
+    for(i=0;i<options.Nlayer;i++) fscanf(initsoil, "%lf",     &moist[i]);
+    for(i=0;i<options.Nlayer;i++) fscanf(initsoil, "%lf",     &ice[i]);
+    for(i=0;i<Nnodes;i++)  fscanf(initsoil, "%lf %lf", &thermdepths[i], &T[i]);
+
+    compute_dz(dz, thermdepths, Nnodes, dp);
+    
+    free((char*)thermdepths);
+
+  }
+
+  else if(!feof(initsoil)) {
+
+    /** Read Initial Soil Thermal Properties for Full Energy Balance Model **/
+
+    thermdepths = (double*)calloc(Nnodes,sizeof(double));
+  
+    fscanf(initsoil, "%i",  &tmpnodes);
+    if(Nnodes!=tmpnodes) {
+      sprintf(ErrStr,"Number of nodes defined in soil thermal initialization file (%i), not equal to number of nodes defined in model (%i).",tmpnodes,Nnodes);
+    }
+    for(i=0;i<2;i++)       fdepth[i] = 0.;
+    for(i=0;i<options.Nlayer;i++) fscanf(initsoil, "%lf",     &moist[i]);
+    for(i=0;i<options.Nlayer;i++) ice[i] = 0.;
+    for(i=0;i<Nnodes;i++)  fscanf(initsoil, "%lf %lf", &thermdepths[i], &T[i]);
+
+    compute_dz(dz, thermdepths, Nnodes, dp);
+
+    free((char*)thermdepths);
+
+  }
+  else {
+
+    /** No Initial Conditions Provided in File **/
+    sprintf(ErrStr,"No initial soil energy balance conditions provided for cell %i, vegetation %i, and snow band %i.  Currently unable to handle this options, check input file.",cellnum,veg,band);
+    nrerror(ErrStr);
+  }
+
+}
+
+void compute_dz(double *dz, double *thermdepths, int Nnodes, double dp) {
+ 
+  char ErrStr[MAXSTRING];
+  int  i, j;
+
+  if(thermdepths[Nnodes-1] != dp) {
+    sprintf(ErrStr,"Thermal solution depth %i (Nnodes-1) must equal thermal damping depth %lf, but is equal to %lf",
+	    Nnodes-1,dp,thermdepths[Nnodes-1]);
+    nrerror(ErrStr);
+  }
+  for(j=Nnodes-1;j>0;j--) {
+    thermdepths[j] -= thermdepths[j-1];
+    thermdepths[j] = (double)((int)(thermdepths[j] * 10000. + 0.5))
+      / 10000.;
+  }
+  for(j=1;j<Nnodes-1;j++) {
+    if((int)(thermdepths[j]*1000.+0.5) 
+       == (int)(thermdepths[j+1]*1000.+0.5))
+      dz[j] = (thermdepths[j] + thermdepths[j+1]) / 2.;
+    else {
+      dz[j] = thermdepths[j];
+      j++;
+      dz[j] = thermdepths[j+1];
+      if((int)((dz[j-1]
+		+dz[j])/2.*1000.+0.5)
+	 != (int)(thermdepths[j]*1000.+0.5)) {
+	sprintf(ErrStr,"Check spacing between thermal layers %i and %i\n",
+		j-1,j);
+	nrerror(ErrStr);
+      }
+    }
+  }
+  dz[0] = thermdepths[1];
+  dz[Nnodes-1] = thermdepths[Nnodes-1];
+
+}

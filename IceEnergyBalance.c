b826470c73b9dddff258a0f211b4d65b86eb099e
cherkaue <>
2001-08-15 22:36:53 +0000

    Source code for release 5.0 beta - dissertation and PILPS-2e

---
+++ IceEnergyBalance.c
@@ -0,0 +1,264 @@
+/*
+ * SUMMARY:      IceEnergyBalance.c - Calculate lake ice energy balance
+ * USAGE:        Part of the lake algorithm
+ *
+ * AUTHOR:       Laura Bowling
+ * ORG:          University of Washington, Department of Civil Engineering
+ * E-MAIL:              nijssen@u.washington.edu
+ * ORIG-DATE:     March 16, 2001
+ * LAST-MOD: Fri Mar  2 18:18:19 2001 by Keith Cherkauer <cherkaue@u.washington.edu>
+ * DESCRIPTION:  Calculate ice energy balance
+ * DESCRIP-END.
+ * FUNCTIONS:    IceEnergyBalance()
+ * COMMENTS:     
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vicNl.h>
+
+#if LAKE_MODEL
+
+#define CH_WATER 4186.8e3
+//#define EPS            0.622    /* ratio of molecular weight of water vapor to
+//                                   that for dry air */
+#define CP          1013.0      /* Specific heat of moist air at constant 
+                                   pressure (J/(kg*C)) */
+/*****************************************************************************
+  Function name: IceEnergyBalance()
+
+  Purpose      : Calculate the surface energy balance for the snow pack
+
+  Required     :
+    double TSurf           - new estimate of effective surface temperature
+    va_list ap            - Argument list initialized by va_start().  For
+                            elements of list and order, see beginning of
+                            routine
+
+  Returns      :
+    double RestTerm        - Rest term in the energy balance
+
+  Modifies     : 
+    double *RefreezeEnergy - Refreeze energy (W/m2) 
+    double *VaporMassFlux  - Mass flux of water vapor to or from the
+                            intercepted snow (m/s)
+
+  Comments     :
+    Reference:  Bras, R. A., Hydrology, an introduction to hydrologic
+                science, Addisson Wesley, Inc., Reading, etc., 1990.
+*****************************************************************************/
+double IceEnergyBalance(double TSurf, va_list ap)
+{
+
+  extern option_struct options;
+
+  const char *Routine = "IceEnergyBalance";
+
+  /* start of list of arguments in variable argument list */
+
+  double Dt;                     /* Model time step (hours) */
+  double Ra;                     /* Aerodynamic resistance (s/m) */
+  double Z;                      /* Reference height (m) */
+  double Displacement;           /* Displacement height (m) */
+  double Z0;                     /* surface roughness height (m) */
+  double Wind;                   /* Wind speed (m/s) */
+  double ShortRad;               /* Net incident shortwave radiation (W/m2) */
+  double LongRadIn;              /* Incoming longwave radiation (W/m2) */
+  double AirDens;                /* Density of air (kg/m3) */
+  double Lv;                     /* Latent heat of vaporization (J/kg3) */
+  double Tair;                   /* Air temperature (C) */
+  double Press;                  /* Air pressure (Pa) */
+  double Vpd;			/* Vapor pressure deficit (Pa) */
+  double EactAir;                /* Actual vapor pressure of air (Pa) */
+  double Rain;                   /* Rain fall (m/timestep) */
+  double SweSurfaceLayer;        /* Snow water equivalent in surface layer (m)
+                                 */ 
+  double SurfaceLiquidWater;     /* Liquid water in the surface layer (m) */
+  double OldTSurf;               /* Surface temperature during previous time
+                                   step */
+  double *RefreezeEnergy;        /* Refreeze energy (W/m2) */
+  double *VaporMassFlux;          /* Mass flux of water vapor to or from the
+                                   intercepted snow */
+  double *AdvectedEnergy;         /* Energy advected by precipitation (W/m2) */
+  double DeltaColdContent;       /* Change in cold content (W/m2) */
+  double Tfreeze;     
+  double AvgCond;
+  double SWconducted;
+  double SnowDepth;  
+  double SnowDensity; 
+  double SurfAttenuation; 
+  double *SnowFlux;		/* Ground Heat Flux (W/m2) */
+  double *LatentHeat;		/* Latent heat exchange at surface (W/m2) */
+  double *SensibleHeat;		/* Sensible heat exchange at surface (W/m2) */
+  double *LongRadOut;
+
+  /* end of list of arguments in variable argument list */
+
+  double Density;                /* Density of water/ice at TMean (kg/m3) */
+  double EsSnow;                 /* saturated vapor pressure in the snow pack
+                                   (Pa)  */  
+  
+  double Ls;                     /* Latent heat of sublimation (J/kg) */
+  double NetRad;			/* Net radiation exchange at surface (W/m2) */
+  double RestTerm;		/* Rest term in surface energy balance
+				   (W/m2) */
+  double  TMean;                /* Average temperature for time step (C) */
+  double qnull;
+
+  /* Assign the elements of the array to the appropriate variables.  The list
+     is traversed as if the elements are doubles, because:
+
+     In the variable-length part of variable-length argument lists, the old
+     ``default argument promotions'' apply: arguments of type double are
+     always promoted (widened) to type double, and types char and short int
+     are promoted to int. Therefore, it is never correct to invoke
+     va_arg(argp, double); instead you should always use va_arg(argp,
+     double). 
+
+     (quoted from the comp.lang.c FAQ list)
+     */
+  Dt                 = (double) va_arg(ap, double);
+  Ra                 = (double) va_arg(ap, double);
+  Z                  = (double) va_arg(ap, double);
+  Displacement       = (double) va_arg(ap, double);
+  Z0                 = (double) va_arg(ap, double);
+  Wind               = (double) va_arg(ap, double);
+  ShortRad           = (double) va_arg(ap, double);
+  LongRadIn          = (double) va_arg(ap, double);
+  AirDens            = (double) va_arg(ap, double);
+  Lv                 = (double) va_arg(ap, double);
+  Tair               = (double) va_arg(ap, double);
+  Press              = (double) va_arg(ap, double);
+  Vpd                = (double) va_arg(ap, double);
+  EactAir            = (double) va_arg(ap, double);
+  Rain               = (double) va_arg(ap, double);
+  SweSurfaceLayer    = (double) va_arg(ap, double);
+  SurfaceLiquidWater = (double) va_arg(ap, double);
+  OldTSurf           = (double) va_arg(ap, double);
+  RefreezeEnergy     = (double *) va_arg(ap, double *);
+  VaporMassFlux      = (double *) va_arg(ap, double *);
+  AdvectedEnergy     = (double *) va_arg(ap, double *);
+  DeltaColdContent   = (double) va_arg(ap, double );
+  Tfreeze              = (double) va_arg(ap, double);
+  AvgCond              = (double) va_arg(ap, double);
+  SWconducted              = (double) va_arg(ap, double);
+   SnowDepth          = (double) va_arg(ap, double);
+  SnowDensity        = (double) va_arg(ap, double);
+  SurfAttenuation    = (double) va_arg(ap, double);
+  SnowFlux         = (double *) va_arg(ap, double *);
+  LatentHeat         = (double *) va_arg(ap, double *);
+  SensibleHeat       = (double *) va_arg(ap, double *);
+  LongRadOut       = (double *) va_arg(ap, double *);
+  
+  /* Calculate active temp for energy balance as average of old and new  */
+  
+/*   TMean = 0.5 * (OldTSurf + TSurf); */
+  TMean = TSurf;
+  Density = RHO_W;
+  
+  /* Correct aerodynamic conductance for stable conditions
+     Note: If air temp >> snow temp then aero_cond -> 0 (i.e. very stable)
+     velocity (vel_2m) is expected to be in m/sec */                        
+  
+  /* Apply the stability correction to the aerodynamic resistance 
+     NOTE: In the old code 2m was passed instead of Z-Displacement.  I (bart)
+     think that it is more correct to calculate ALL fluxes at the same
+     reference level */
+
+
+  if (Wind > 0.0)
+    Ra /= StabilityCorrection(Z, 0.f, TMean, Tair, Wind, Z0);
+    /*Ra /= StabilityCorrection(2.f, 0.f, TMean, Tair, Wind, Z0);*/
+  else
+    Ra = HUGE_RESIST;
+
+  /* Calculate longwave exchange and net radiation */
+ 
+  *LongRadOut = LongRadIn - STEFAN * (TMean+273.15) * (TMean+273.15) 
+    * (TMean+273.15) * (TMean+273.15);
+  NetRad = ShortRad + *LongRadOut;
+  
+  /* Calculate the sensible heat flux */
+
+  *SensibleHeat = AirDens * CP * (Tair - TMean)/Ra;
+
+  /* Calculate the mass flux of ice to or from the surface layer */
+ 
+  /* Calculate the saturated vapor pressure in the snow pack, 
+     (Equation 3.32, Bras 1990) */
+
+  EsSnow = svp(TMean) /* * 1000. */;
+
+/*   EsSnow = 610.78 * exp((double)((17.269 * TMean) / (237.3 + TMean))); */
+
+/*   if (TMean < 0.0) */
+/*     EsSnow *= 1.0 + .00972 * TMean + .000042 * pow((double)TMean,(double)2.0); */
+  
+  *VaporMassFlux = AirDens * (EPS/Press) * (EactAir - EsSnow)/Ra;
+  *VaporMassFlux /= Density;
+  if (Vpd == 0.0 && *VaporMassFlux < 0.0)
+    *VaporMassFlux = 0.0;
+  
+  /* Calculate latent heat flux */
+ 
+  if (TMean >= 0.0) {
+    /* Melt conditions: use latent heat of vaporization */
+    *LatentHeat = Lv * *VaporMassFlux * Density;
+  }
+  else {
+    /* Accumulation: use latent heat of sublimation (Eq. 3.19, Bras 1990 */
+    Ls = (677. - 0.07 * TMean) * JOULESPCAL * GRAMSPKG;
+    *LatentHeat = Ls * *VaporMassFlux * Density;
+  }
+  
+  /* Calculate advected heat flux from rain 
+     WORK IN PROGRESS:  Should the following read (Tair - Tsurf) ?? */
+  
+  // Temporary fix for lake model.
+  *AdvectedEnergy = (CH_WATER * Tair * Rain) / (Dt*SECPHOUR);
+  //*AdvectedEnergy = 0.0;
+  
+  /* Calculate change in cold content */
+  
+  /* Changes for lake model start here. */
+  qnull = (1/AvgCond)*(Tfreeze - TMean + SWconducted);
+  *SnowFlux = qnull + ShortRad - DeltaColdContent;
+
+  //*DeltaColdContent = CH_ICE * SweSurfaceLayer * (TSurf - OldTSurf)/
+  //(Dt * SECPHOUR);
+  /* Calculate Ground Heat Flux */
+  //  if(SnowDepth>0.) {
+  //  *GroundFlux = 2.9302e-6 * SnowDensity * SnowDensity
+  //     * (TGrnd - TMean) / SnowDepth;
+  //}
+  //else *GroundFlux=0;
+  //*DeltaColdContent -= *GroundFlux;
+  /* Changes for lake model end here. */
+  
+  /* Calculate net energy exchange at the snow surface */
+  
+  RestTerm = NetRad + *SensibleHeat + *LatentHeat + *AdvectedEnergy 
+            + qnull;
+
+  *RefreezeEnergy = (SurfaceLiquidWater * Lf * Density)/(Dt * SECPHOUR);
+
+  /* Melting, or partially refreeze surface water. */
+  if (TSurf == 0.0 && RestTerm > -(*RefreezeEnergy)) {
+    *RefreezeEnergy = -RestTerm;  /* available energy input over cold content
+                                    used to melt, i.e. Qrf is negative value
+                                    (energy out of pack)*/ 
+    RestTerm = 0.0;
+  }
+  else {                         /* Pack is getting colder. */
+    RestTerm += *RefreezeEnergy; /* add this positive value to the pack */
+  }
+  
+  return RestTerm;
+}
+
+#undef CH_WATER
+#undef EPS
+#undef CP
+
+#endif // LAKE_MODEL
---
+++ LAKE.h
@@ -0,0 +1,86 @@
+#if LAKE_MODEL
+
+//#ifndef LAKE_SET
+
+#define LAKE_SET
+#define TMELT 0.0
+#define EMICE 0.97      /* Ice emissivity */
+#define EMH2O 0.98
+#define RHOSNOW   330.  /* densities of water and snow */
+#define RHOICE   917.   /* ice density*/
+#define rhosurf 1.275   /* surface air density */
+#define JOULESPCAL 4.1868
+#define GRAMSPKG 1000.
+#define SURF   .6     /* surface thickness for E-B computation */
+#define FRACMIN  0.01   /* min ice thickness in meters */
+#define FRACLIM   0.02  /* lower limit on fractional ice cover */
+#define CPW_ICE   4200. /* specific heat of ice */
+#define DM 1.38889E-07  /* molecular diffusivity of water */
+#define SNOWCRIT   0.05  /* for albedo, in m */
+//#define G 9.80616
+#define ZWATER 0.004
+#define ZSNOW 0.005
+#define CONDI 2.3        /* thermal conductivity of ice */
+#define CONDS 0.31       /* thermal conductivity of snow */ 
+#define MAX_LAKE_NODES 20
+#define lamisw 1.5 
+#define lamilw 20
+#define lamwsw 2.3 
+#define lamwlw .005
+#define lamssw 6.0
+#define lamslw 20.
+#define  a1 0.7        /* Percent of radiation in visible band. */
+#define  a2 0.3        /* Percent of radiation in infrared band. */
+#define QWTAU 86400./2.   /* D. Pollard sub-ice time constant. */
+#define RADIUS 6371.228 /* Earth radius in km. */
+
+//#endif // LAKE_SET
+
+/*** Subroutine prototypes ***/
+
+double adjflux(double, double, double ,double, double, double, double,
+	       double, double, double, double *, double *);
+void alblake(double, double, float *, float *, float *, double, double, 
+	     int, int *, double);
+void alloc_atmos(int, atmos_data_struct **);
+double calc_density(double);
+void colavg (double *, double *, double *, float, double *, int, double);
+float dragcoeff(float, double, double);
+void eddy (int, double, double * , double *, double *, double, int, double);
+void energycalc(double *, double *, int, double, double *, double *, double *);
+double get_dist(double, double, double, double);
+void iceform (double *,double *,double ,double,double *,int, int, double, double *, double *, double *, double *);
+void icerad(double,double ,double,double *, double *,double *);
+void initialize_lake(lake_var_struct *, lake_con_struct, snow_data_struct *, 
+		     double);
+void lakeice(double *, double, double, double *, double, double *,int, 
+	     double, double, double *, double, double);
+void lakemain(atmos_data_struct, lake_var_struct *, lake_con_struct, double, 
+	      soil_con_struct, int, energy_bal_struct *, snow_data_struct *, 
+	      int, int, double);
+void latsens(double,double, double, double, double, double, double, double,
+	     double *, double *, double);
+float lkdrag(float, double, double, double, double);
+lake_con_struct read_lakeparam(FILE *, soil_con_struct, float);
+void rhoinit(double *, double);
+void solve_lake(double, double, double, double, double, double, double, 
+		double, double, lake_var_struct *, lake_con_struct, 
+		soil_con_struct, int, int, energy_bal_struct *, 
+		snow_data_struct *, double);
+double specheat (double);
+void temp_area(double, double, double, double *, double *, double *, double *, int, double *, int, double, double*, double *, double *);
+void tracer_mixer(double *, int *, int, double*, int, double, double *);
+void tridia(int, double *, double *, double *, double *, double *);
+void water_balance (lake_var_struct *, lake_con_struct, int);
+double func_lake_energy_balance(double, va_list);
+double solve_surf_energy_bal(double Tsurf, ...);
+void ice_melt(double, double, double, snow_data_struct *, lake_var_struct *, int, double, double, double, double, double, double, double, double, double, double, double, double, double, double, double *, double *, double *, double *, double *, double *, double *, double *, double *, double);
+double CalcIcePackEnergyBalance(double Tsurf, ...);
+double func_lake_energy_bal(double , va_list);
+double IceEnergyBalance(double, va_list);
+double ErrorPrintIcePackEnergyBalance(double, va_list);
+double ErrorIcePackEnergyBalance(double Tsurf, ...);
+void water_energy_balance(int, double*, double*, int,int,double, double ,double ,double ,double ,double ,double ,double ,double,double,double ,double ,double *,double *,double *,double*,double *, double *,double *,double ,double *,double *, double *);
+void water_under_ice(int, double,  double, double *, double *, double, int, double, double, double *, double *, double *, double *, int, double, double, double);
+
+#endif // LAKE_MODEL

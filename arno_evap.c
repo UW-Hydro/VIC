33ffaa523b09dee9380d81a50bf407c33bb325e5
tbohn <>
2004-06-14 19:50:34 +0000

    1. Moved unit conversion of moist_resid back inside distributed precipitation loop in such a way that it does not get multiplied by D1 * 1000 twice.
    2. Changed logic of evap limit check to avoid creating spurious
    condensation.  Previously, when liquid moisture < residual
    moisture, (liquid moisture - residual moisture) would be
    negative.  Any non-negative evap would be greater than this,
    resulting in evap getting set to (liquid moisture - residual
    moisture), which would be negative (i.e. condensation).
    This artificially created condensation in whatever amount
    necessary to bring liquid moisture up to residual, causing
    1) large latent heat flux, 2) incorrect surface temperatures,
    3) occasional inability for calc_surf_energy_bal to converge
    in root_brent, and 4) spuriously high runoff and baseflow.
    Now there is an added condition that liquid moisture > residual
    moisture for evap to be capped at (liquid moisture - residual
    moisture).  In addition, the previous logic for capping evap
    involved an incorrect calculation of the soil's ice content.
    Since the new logic doesn't require calculation of ice content,
    this calculation has been removed altogether.

--- arno_evap.c
+++ arno_evap.c
@@ -22,7 +22,27 @@ static char vcid[] = "$Id$";
   2-24-03   moved unit conversion of moist_resid outside of distributed
             precipitation loop.  Moist_resid was being multiplied by
             D! * 1000 twice for the dry fraction.                 KAC
-       
+  04-Jun-04 Moved unit conversion of moist_resid back inside distributed
+	    precipitation loop in such a way that it does not get multiplied
+	    by D1 * 1000 twice.					TJB
+  04-Jun-04 Changed logic of evap limit check to avoid creating spurious
+	    condensation.  Previously, when liquid moisture < residual
+	    moisture, (liquid moisture - residual moisture) would be
+	    negative.  Any non-negative evap would be greater than this,
+	    resulting in evap getting set to (liquid moisture - residual
+	    moisture), which would be negative (i.e. condensation).
+	    This artificially created condensation in whatever amount
+	    necessary to bring liquid moisture up to residual, causing
+	    1) large latent heat flux, 2) incorrect surface temperatures,
+	    3) occasional inability for calc_surf_energy_bal to converge
+	    in root_brent, and 4) spuriously high runoff and baseflow.
+	    Now there is an added condition that liquid moisture > residual
+	    moisture for evap to be capped at (liquid moisture - residual
+	    moisture).	In addition, the previous logic for capping evap
+	    involved an incorrect calculation of the soil's ice content.
+	    Since the new logic doesn't require calculation of ice content,
+	    this calculation has been removed altogether.		TJB
+
 ****************************************************************************/
 
 double arno_evap(layer_data_struct *layer_wet,
@@ -64,7 +84,6 @@ double arno_evap(layer_data_struct *layer_wet,
   double ratio,as;
   double Epot;		/* potential bare soil evaporation */
   double moist;
-  double ice;
   double evap;
   double max_infil;
   double Evap;
@@ -76,8 +95,6 @@ double arno_evap(layer_data_struct *layer_wet,
 
   Evap = 0;
 
-  moist_resid *= D1 * 1000.;
-
   for(dist=0;dist<Ndist;dist++) {
 
     if(dist>0) {
@@ -88,135 +105,123 @@ double arno_evap(layer_data_struct *layer_wet,
       layer = layer_wet;
     }
 
+    /* moist = liquid soil moisture */
 #if SPATIAL_FROST
     moist = 0;
-    ice   = 0;
     for ( frost_area = 0; frost_area < FROST_SUBAREAS; frost_area++ ) {
       moist += (layer[0].moist - layer[0].ice[frost_area])
 	* frost_fract[frost_area];
-      ice   += layer[0].ice[frost_area] * frost_fract[frost_area];
     }
 #else
     moist = layer[0].moist - layer[0].ice;
-    ice   = layer[0].moist - layer[0].ice;
 #endif
     if ( moist > max_moist ) moist = max_moist;
 
-    if ( moist > 0 ) {
-    
-      /* Calculate the potential bare soil evaporation (mm/time step) */
-    
-      Epot = penman(rad, vpd, ra, 0.0, 0.0, 1.0, 1.0, 
-	  	    air_temp, net_short, elevation, 0) * delta_t / SEC_PER_DAY;
-    
-      /**********************************************************************/
-      /*  Compute temporary infiltration rate based on given soil_moist.    */
-      /**********************************************************************/
-      max_infil = (1.0 + b_infilt) * max_moist;
-      if(b_infilt == -1.0)
-        tmp = max_infil;
+    /* Calculate the potential bare soil evaporation (mm/time step) */
+  
+    Epot = penman(rad, vpd, ra, 0.0, 0.0, 1.0, 1.0, 
+ 	    air_temp, net_short, elevation, 0) * delta_t / SEC_PER_DAY;
+  
+    /**********************************************************************/
+    /*  Compute temporary infiltration rate based on given soil_moist.    */
+    /**********************************************************************/
+    max_infil = (1.0 + b_infilt) * max_moist;
+    if(b_infilt == -1.0)
+      tmp = max_infil;
+    else {
+      ratio = 1.0 - (moist) / (max_moist);
+      /*****if(ratio < SMALL && ratio > -SMALL) ratio = 0.;*****/
+      if(ratio > 1.0) {
+	printf("\n  ERROR: SOIL RATIO GREATER THAN 1.0\n");
+	printf("moisture %f   max_moisture %f -> ratio = %f\n",
+	       moist,max_moist,ratio);
+	exit(0);
+      }
       else {
-        ratio = 1.0 - (moist) / (max_moist);
-        /*****if(ratio < SMALL && ratio > -SMALL) ratio = 0.;*****/
-        if(ratio > 1.0) {
-	  printf("\n  ERROR: SOIL RATIO GREATER THAN 1.0\n");
-  	  printf("moisture %f   max_moisture %f -> ratio = %f\n",
+	if(ratio < 0.0) {
+	  printf("\n  ERROR: SOIL RATIO LESS THAN 0.0\n");
+	  printf("moisture %f   max_moisture %f -> ratio = %e\n",
 	         moist,max_moist,ratio);
 	  exit(0);
-        }
-        else {
-	  if(ratio < 0.0) {
-	    printf("\n  ERROR: SOIL RATIO LESS THAN 0.0\n");
-	    printf("moisture %f   max_moisture %f -> ratio = %e\n",
-		   moist,max_moist,ratio);
-	    exit(0);
-	  }
-	  else
-	    ratio = pow(ratio,(1.0 / (b_infilt + 1.0)));
-        }
-        tmp = max_infil*(1.0 - ratio);
+	}
+	else
+	  ratio = pow(ratio,(1.0 / (b_infilt + 1.0)));
       }
+      tmp = max_infil*(1.0 - ratio);
+    }
+  
+    /**********************************************************************/
+    /* Evaporate at potential rate, i.e., Eq.(10) in Liang's derivation.  */
+    /**********************************************************************/
+  
+    if(tmp >= max_infil)
+      evap = Epot;     
+    else {                 
     
-      /**********************************************************************/
-      /* Evaporate at potential rate, i.e., Eq.(10) in Liang's derivation.  */
-      /**********************************************************************/
+      /********************************************************************/
+      /*  Compute As. 'As' is % area saturated, '1-As' is % area          */
+      /*  that is unsaturated.                                            */
+      /********************************************************************/
     
-      if(tmp >= max_infil)
-        evap = Epot;     
-      else {                 
-      
-        /********************************************************************/
-        /*  Compute As. 'As' is % area saturated, '1-As' is % area          */
-        /*  that is unsaturated.                                            */
-        /********************************************************************/
-      
-        ratio = tmp/max_infil; 
-        ratio = 1.0 - ratio;
-      
-        if(ratio > 1.0) {
-	  printf("\n ARNO ERROR: EVAP RATIO GREATER THAN 1.0");
+      ratio = tmp/max_infil; 
+      ratio = 1.0 - ratio;
+    
+      if(ratio > 1.0) {
+	printf("\n ARNO ERROR: EVAP RATIO GREATER THAN 1.0");
+	exit(0);
+      }
+      else {
+	if(ratio < 0.0) {
+	  printf("\n ARNO ERROR: EVAP RATIO LESS THAN 0.0");
 	  exit(0);
-        }
-        else {
-	  if(ratio < 0.0) {
-	    printf("\n ARNO ERROR: EVAP RATIO LESS THAN 0.0");
-	    exit(0);
-	  }
-	  else {
-	    if(ratio != 0.0)
-	      ratio = pow(ratio,b_infilt);
-	  }
-        }
-      
-        as = 1 - ratio;
-      
-        /********************************************************************/
-        /*  Compute the beta function in the ARNO evaporation model using   */
-        /*  the first 30 terms in the power expansion expression.           */
-        /********************************************************************/ 
-      
-        ratio = pow(ratio,(1.0/b_infilt));
-      
-        dummy = 1.0;
-        for(num_term=1;num_term<=30;num_term++) {
-	  tmpsum = ratio;
-	  for ( i = 1; i < num_term; i++ ) tmpsum *= ratio;
-	  dummy += b_infilt * tmpsum / (b_infilt + num_term);
-        }
-      
-        beta_asp = as+(1.0-as)*(1.0-ratio)*dummy;
-        evap = Epot*beta_asp;
+	}
+	else {
+	  if(ratio != 0.0)
+	    ratio = pow(ratio,b_infilt);
+	}
       }
-	
-      /***********************************************************************/
-      /*  Evaporation cannot exceed available soil moisture.                 */
-      /*  Evaporation second soil layer = 0.0                                */
-      /***********************************************************************/
+    
+      as = 1 - ratio;
+    
+      /********************************************************************/
+      /*  Compute the beta function in the ARNO evaporation model using   */
+      /*  the first 30 terms in the power expansion expression.           */
+      /********************************************************************/ 
+    
+      ratio = pow(ratio,(1.0/b_infilt));
+    
+      dummy = 1.0;
+      for(num_term=1;num_term<=30;num_term++) {
+	tmpsum = ratio;
+	for ( i = 1; i < num_term; i++ ) tmpsum *= ratio;
+	dummy += b_infilt * tmpsum / (b_infilt + num_term);
+      }
+    
+      beta_asp = as+(1.0-as)*(1.0-ratio)*dummy;
+      evap = Epot*beta_asp;
+    }
 
-      if ( ice > 0 ) {
-        if ( ice > moist_resid ) {
- 	  // ice content greater than wilting point can use all unfrozen moist
-          if ( evap > moist ) evap = moist;
-        }
-        else {
-	  // ice content less than wilting point restrict loss of unfrozen moist
-          if ( evap > ( moist + ice ) - moist_resid )
-            evap = ( moist + ice ) -  moist_resid;
+    /***********************************************************************/
+    /*  Evaporation cannot exceed available soil moisture.                 */
+    /*  Evaporation second soil layer = 0.0                                */
+    /***********************************************************************/
+
+    /* only consider positive evaporation; we won't put limits on condensation */
+    if (evap > 0.0) {
+      if (moist > moist_resid * D1 * 1000.) {
+        /* there is liquid moisture available; cap evap at available liquid moisture */
+        if (evap > moist -  moist_resid * D1 * 1000.) {
+          evap = moist -  moist_resid * D1 * 1000.;
         }
       }
       else {
-        if ( evap > ( moist + ice ) - moist_resid)
-          evap = ( moist + ice ) -  moist_resid;
+        /* no moisture available; cap evap at 0 */
+        evap = 0.0;
       }
-
-      layer[0].evap = evap;
-      Evap += evap /1000. / delta_t * mu;
-
     }
-    else {
-      Evap = 0;
-      layer[0].evap = 0;
-    }
+
+    layer[0].evap = evap;
+    Evap += evap / 1000. / delta_t * mu;
 
   }
   

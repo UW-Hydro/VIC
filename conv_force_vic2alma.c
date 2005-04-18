069732d59d255968babe8aa1fe6e15055a5d1389
vicadmin <vicadmin@hydro.washington.edu>
2005-04-18 06:23:03 +0000

    Now rain and snow fractions vary linearly between MIN_RAIN_TEMP and MAX_SNOW_TEMP.

--- conv_force_vic2alma.c
+++ conv_force_vic2alma.c
@@ -12,20 +12,32 @@ void conv_force_vic2alma(atmos_data_struct *atmos, int rec, int j, int dt, atmos
   compliant forcing variables and stores them in the structure atmos_alma.
   
   modifications:
+  2005-04-17 Rain and Snow fractions now vary linearly between MIN_RAIN_TEMP
+	     and MAX_SNOW_TEMP.						TJB
 
 **********************************************************************/
 {
+  extern global_param_struct global_param;
+
+  double MIN_RAIN_TEMP;
+  double MAX_SNOW_TEMP;
+
+  MIN_RAIN_TEMP = global_param.MIN_RAIN_TEMP;
+  MAX_SNOW_TEMP = global_param.MAX_SNOW_TEMP;
 
   atmos_alma->SWdown = atmos[rec].shortwave[j];
   atmos_alma->LWdown = atmos[rec].longwave[j];
   atmos_alma->Tair = atmos[rec].air_temp[j] + KELVIN;
   atmos_alma->Qair = atmos[rec].vp[j] / atmos[rec].pressure[j];
   atmos_alma->Psurf = atmos[rec].pressure[j];
-  // Setting upper limit of air temperature for snowfall at 275.65K (2.5 C),
-  // based on reference in CLM model to Fig. 1, Plate 3-1, of Snow Hydrology (1956).
-  if (atmos[rec].air_temp[j] > 2.5) {
+  if (atmos[rec].air_temp[j] >= MAX_SNOW_TEMP) {
     atmos_alma->Rainf = atmos[rec].prec[j] / (dt * 3600);
     atmos_alma->Snowf = 0.0;
+  }
+  else if (atmos[rec].air_temp[j] > MIN_RAIN_TEMP) {
+    atmos_alma->Rainf = ((atmos[rec].air_temp[j]-MIN_RAIN_TEMP) / (MAX_SNOW_TEMP-MIN_RAIN_TEMP))
+                        * atmos[rec].prec[j] / (dt * 3600);
+    atmos_alma->Snowf = atmos[rec].prec[j] / (dt * 3600) - atmos_alma->Rainf;
   }
   else {
     atmos_alma->Rainf = 0.0;

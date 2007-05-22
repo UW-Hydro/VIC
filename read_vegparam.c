babddb9fcb82fc942143532b5520048802297a57
vicadmin <vicadmin@hydro.washington.edu>
2007-05-22 21:29:50 +0000

    Check for correct number of root zones, blowing and LAI in veg file.

--- read_vegparam.c
+++ read_vegparam.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vicNl.h>
+#include <string.h>
 
 static char vcid[] = "$Id$";
 
@@ -27,8 +28,12 @@ veg_con_struct *read_vegparam(FILE *vegparam,
            function, then the values cacluated in read_veglib.c are
            left unchanged.                                   DP & KAC
   2006-Nov-07 Allocates MaxVeg+1 veg tiles.  TJB
+  2007-May-11 Changed some 'fscanf' statements to 'fgets' and 'sscanf' 
+           to count rootzone and BLOWING fields. Also tests for fetch < 1.GCT
 **********************************************************************/
 {
+
+  void ttrim( char *string );
   extern veg_lib_struct *veg_lib;
   extern option_struct   options;
 #if LINK_DEBUG
@@ -38,10 +43,16 @@ veg_con_struct *read_vegparam(FILE *vegparam,
   veg_con_struct *temp;
   int             vegcel, i, j, vegetat_type_num, skip, veg_class;
   int             MaxVeg;
+  int             NF, NTOT;
   float           depth_sum;
   float           sum;
   char            str[500];
   char            ErrStr[MAXSTRING];
+  char            line[MAXSTRING];
+  char            tmpline[MAXSTRING];
+  const char      delimiters[] = " ";
+  char            *token;
+  char            *vegarr[500];
 
   if(options.GLOBAL_LAI) skip=2;
   else skip=1;
@@ -50,11 +61,17 @@ veg_con_struct *read_vegparam(FILE *vegparam,
   rewind(vegparam);
 #endif  
     
-  while ((fscanf(vegparam, "%d %d", &vegcel, &vegetat_type_num)) == 2 &&
-          vegcel != gridcel) {
-    for (i = 0; i <= vegetat_type_num * skip; i++)
-      fgets(str, 500, vegparam);
+
+
+  while ( ( fscanf(vegparam, "%d %d", &vegcel, &vegetat_type_num) == 2 ) && vegcel != gridcel ){
+    for (i = 0; i <= vegetat_type_num * skip; i++){
+      if ( fgets(str, 500, vegparam) == NULL ){
+        sprintf(ErrStr,"ERROR unexpected EOF for cell %i while reading root zones and LAI\n",vegcel);
+        nrerror(ErrStr);
+      }
+    }
   }
+  fgets(str, 500, vegparam); // read newline at end of veg class line to advance to next line
   if (vegcel != gridcel) {
     fprintf(stderr, "Error in vegetation file.  Grid cell %d not found\n",
             gridcel);
@@ -82,13 +99,42 @@ veg_con_struct *read_vegparam(FILE *vegparam,
     temp[i].zone_depth = calloc(options.ROOT_ZONES,sizeof(float));
     temp[i].zone_fract = calloc(options.ROOT_ZONES,sizeof(float));
     temp[i].vegetat_type_num = vegetat_type_num;
-    fscanf(vegparam, "%d",  &temp[i].veg_class);
-    fscanf(vegparam, "%lf", &temp[i].Cv);
-    
+
+    // Read the root zones line
+    if ( fgets( line, MAXSTRING, vegparam ) == NULL ){
+      sprintf(ErrStr,"ERROR unexpected EOF for cell %i while reading vegetat_type_num %d\n",vegcel,vegetat_type_num);
+      nrerror(ErrStr);
+    }
+    strcpy(tmpline, line);
+    ttrim( tmpline );
+    token = strtok (tmpline, delimiters);    /*  token => veg_class, move 'line' pointer to next field */  
+    NF = 0;
+    vegarr[NF] = calloc( 500, sizeof(char));
+    strcpy(vegarr[NF],token);
+    NF++;
+
+    while( ( token = strtok (NULL, delimiters)) != NULL ){
+      vegarr[NF] = calloc( 500, sizeof(char));      
+      strcpy(vegarr[NF],token);
+      NF++;
+    }
+
+    NTOT = 2 + 2 * options.ROOT_ZONES;  /* Number of expected fields this line */
+    if( options.BLOWING ){
+      NTOT += 3;
+    }
+    if ( NF != NTOT ) {
+      sprintf(ErrStr,"ERROR - cell %d - expecting %d fields but found %d in veg line %s\n",gridcel,NTOT, NF, line);
+      nrerror(ErrStr);
+    }
+
+    temp[i].veg_class = atoi( vegarr[0] );
+    temp[i].Cv = atof( vegarr[1] );
     depth_sum = 0;
     sum = 0.;
     for(j=0;j<options.ROOT_ZONES;j++) {
-      fscanf(vegparam,"%f %f",&temp[i].zone_depth[j], &temp[i].zone_fract[j]);
+      temp[i].zone_depth[j] = atof( vegarr[2 + j*2] );
+      temp[i].zone_fract[j] = atof( vegarr[3 + j*2] );
       depth_sum += temp[i].zone_depth[j];
       sum += temp[i].zone_fract[j];
     }
@@ -104,10 +150,17 @@ veg_con_struct *read_vegparam(FILE *vegparam,
     }
 
     if(options.BLOWING) {
-       fscanf(vegparam,"%f %f %f",&temp[i].sigma_slope, &temp[i].lag_one, &temp[i].fetch);
+      j = 2 * options.ROOT_ZONES;
+      temp[i].sigma_slope = atof( vegarr[2 + j] );
+      temp[i].lag_one = atof( vegarr[3 + j] );
+      temp[i].fetch = atof( vegarr[4 + j]) ;
       if( temp[i].sigma_slope <= 0. || temp[i].lag_one <= 0.) {
-      sprintf(str,"Deviation of terrain slope must be greater than 0.");
-       nrerror(str);
+        sprintf(str,"Deviation of terrain slope must be greater than 0.");
+        nrerror(str);
+      }
+      if( temp[i].fetch < 1.0  ) {
+	sprintf(str,"ERROR - BLOWING parameter fetch should be >> 1 but cell %i has fetch = %.2f\n", gridcel, temp[i].fetch );
+        nrerror(str);
       }
     }
 
@@ -125,12 +178,34 @@ veg_con_struct *read_vegparam(FILE *vegparam,
     temp[0].Cv_sum += temp[i].Cv;
 
     if ( options.GLOBAL_LAI )
-      for ( j = 0; j < 12; j++ ) {
-	fscanf(vegparam,"%lf",&veg_lib[temp[i].veg_class].LAI[j]);
-	veg_lib[temp[i].veg_class].Wdmax[j] = 
+      // Read the LAI line
+      if ( fgets( line, MAXSTRING, vegparam ) == NULL ){
+        sprintf(ErrStr,"ERROR unexpected EOF for cell %i while reading LAI for vegetat_type_num %d\n",vegcel,vegetat_type_num);
+        nrerror(ErrStr);
+      }      
+      NF = 0;
+      strcpy(tmpline, line);
+      ttrim( tmpline );
+      token = strtok (tmpline, delimiters); 
+      strcpy(vegarr[NF],token);
+      NF++;
+ 
+      while( ( token = strtok (NULL, delimiters)) != NULL ){
+        vegarr[NF] = calloc( 500, sizeof(char));      
+        strcpy(vegarr[NF],token);
+        NF++;
+     }
+     NTOT = 12; /* For LAI */
+     if ( NF != NTOT ) {
+       sprintf(ErrStr,"ERROR - cell %d - expecting %d LAI values but found %d in line %s\n",gridcel, NTOT, NF, line);
+       nrerror(ErrStr);
+     }
+
+     for ( j = 0; j < 12; j++ ) {
+       veg_lib[temp[i].veg_class].LAI[j] = atof( vegarr[j] );
+       veg_lib[temp[i].veg_class].Wdmax[j] = 
 	  LAI_WATER_FACTOR * veg_lib[temp[i].veg_class].LAI[j];
       }
-    
   }
   if(temp[0].Cv_sum>1.0){
     fprintf(stderr,"WARNING: Cv exceeds 1.0 at grid cell %d, fractions being adjusted to equal 1\n", gridcel);
@@ -147,4 +222,21 @@ veg_con_struct *read_vegparam(FILE *vegparam,
   return temp;
 } 
 
+
+
+
+
+
+/* trim trailing newlines */
+
+#define END '\0'
+#define NEW '\n'
+
+void ttrim( char *c ) 
+{
+  while( (*c++ != END) );
+    --c;
+  for( ; *--c == NEW; *c = END );
+
+}
 

69393d26a002124bb0869fa853ee407cb208d282
vicadmin <vicadmin@hydro.washington.edu>
2007-09-19 00:06:30 +0000

    Handling of cells missing from snowband file

--- read_snowband.c
+++ read_snowband.c
@@ -19,6 +19,9 @@ void read_snowband(FILE    *snowband,
   precipitation fraction for use with the snow model.
 
   04-25-03 Modified to allocate treeline variable.            KAC
+  2007-Sep-18 Modified to output a warning and set snow bands to 1
+	      if no snowband information is present in the snowband
+	      file for the current grid cell.				KAC via TJB
 
 **********************************************************************/
 {
@@ -42,6 +45,19 @@ void read_snowband(FILE    *snowband,
   if (*Tfactor == NULL || *Pfactor == NULL || *AreaFract == NULL) 
     nrerror("Memory allocation failure in read_snowband");
 
+  if ( Nbands <= 0 ) {
+    sprintf(ErrStr,"Number of snow elevation bands must be > 0 (%d)",Nbands);
+    nrerror(ErrStr);
+  }
+
+  /** Set default values for factors to use unmodified forcing data **/
+  for (band = 0; band < Nbands; band++) {
+    (*AreaFract)[band] = 0.;
+    (*Tfactor)[band]   = 0.;
+    (*Pfactor)[band]   = 1.;
+  }
+  (*AreaFract)[0] = 1.;
+
   if ( Nbands > 1 ) {
 
     /** Find Current Grid Cell in SnowBand File **/
@@ -54,10 +70,12 @@ void read_snowband(FILE    *snowband,
       fgets(ErrStr,MAXSTRING,snowband);
       fscanf(snowband, "%d", &cell);
     }
+
     if ( feof(snowband) ) {
-      sprintf(ErrStr,"Cannot find current gridcell (%d) in snow band file",
-	      gridcell);
-      nrerror(ErrStr);
+      fprintf(stderr, "WARNING: Cannot find current gridcell (%i) in snow elevation band file; setting cell to have one elevation band.\n",
+              gridcell);
+      /** 1 band is the default; no action necessary **/
+      return;
     }
 
     /** Read Area Fraction **/
@@ -65,7 +83,7 @@ void read_snowband(FILE    *snowband,
     for( band = 0; band < Nbands; band++ ) {
       fscanf(snowband, "%lf", &area_fract);
       if(area_fract<0) {
-	sprintf(ErrStr,"Negative snow band area fraction (%f) read from file", 
+	sprintf(ErrStr,"Negative snow elevation band area fraction (%f) read from file", 
 		area_fract);
 	nrerror(ErrStr);
       }
@@ -73,7 +91,7 @@ void read_snowband(FILE    *snowband,
       total              += area_fract;
     }
     if ( total != 1. ) {
-      fprintf(stderr,"WARNING: Sum of the snow band area fractions does not equal 1 (%f), dividing each fraction by the sum\n",
+      fprintf(stderr,"WARNING: Sum of the snow elevation band area fractions does not equal 1 (%f), dividing each fraction by the sum\n",
 	      total);
       for ( band = 0; band < options.SNOW_BAND; band++ ) 
 	(*AreaFract)[band] /= total;
@@ -83,7 +101,7 @@ void read_snowband(FILE    *snowband,
     for ( band = 0; band < Nbands; band++ ) {
       fscanf(snowband, "%lf", &band_elev);
       if ( band_elev < 0 ) {
-	fprintf(stderr,"Negative snow band elevation (%f) read from file\n", 
+	fprintf(stderr,"Negative snow elevation band elevation (%f) read from file\n", 
 		band_elev);
       }
       (*Tfactor)[band] = ( elev - band_elev ) / 1000. * T_lapse;
@@ -94,12 +112,12 @@ void read_snowband(FILE    *snowband,
     for ( band = 0; band < options.SNOW_BAND; band++ ) {
       fscanf(snowband, "%lf", &prec_frac);
       if(prec_frac<0) {
-	sprintf(ErrStr,"Snow band precipitation fraction (%f) must be between 0 and 1", 
+	sprintf(ErrStr,"Snow elevation band precipitation fraction (%f) must be between 0 and 1", 
 		prec_frac);
 	nrerror(ErrStr);
       }
       if ( prec_frac > 0 && (*AreaFract)[band] == 0 ) {
-	sprintf(ErrStr,"Snow band precipitation fraction (%f) should be 0 when the area fraction is 0. (band = %d)", 
+	sprintf(ErrStr,"Snow elevation band precipitation fraction (%f) should be 0 when the area fraction is 0. (band = %d)", 
 		prec_frac, band);
 	nrerror(ErrStr);
       }
@@ -107,7 +125,7 @@ void read_snowband(FILE    *snowband,
       total += prec_frac;
     }
     if ( total != 1. ) {
-      fprintf(stderr,"WARNING: Sum of the snow band precipitation fractions does not equal %d (%f), dividing each fraction by the sum\n",
+      fprintf(stderr,"WARNING: Sum of the snow elevation band precipitation fractions does not equal %d (%f), dividing each fraction by the sum\n",
 	      1, total);
       for(band = 0; band < options.SNOW_BAND; band++) 
 	(*Pfactor)[band] /= total;
@@ -118,18 +136,7 @@ void read_snowband(FILE    *snowband,
       else 
 	(*Pfactor)[band]  = 0.;
     }
-  }
 
-  else if ( Nbands == 1 ) {
-    /** If no snow bands, set factors to use unmodified forcing data **/
-    (*AreaFract)[0] = 1.;
-    (*Tfactor)[0]   = 0.;
-    (*Pfactor)[0]   = 1.;
-  }
-
-  else {
-    sprintf(ErrStr,"Number of snow elevation bands must be > 0 (%d)",Nbands);
-    nrerror(ErrStr);
   }
 
 } 

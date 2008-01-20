131a9d4c942f1b66e4d5a19c164a1d73bba34363
vicadmin <vicadmin@hydro.washington.edu>
2008-01-20 05:59:13 +0000

    Final release of 4.0.6.

--- global.h
+++ global.h
@@ -6,7 +6,7 @@
   29-Oct-03 Added version string and removed unused options from
 	    optstring.						TJB
 **********************************************************************/
-char *version = "4.0.6 Beta Release 1";
+char *version = "4.0.6";
 
 char *optstring = "g:vo";
 

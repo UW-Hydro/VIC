c8d84e7ed98a3e095a6451223964717fb7bf612f
vicadmin <vicadmin@hydro.washington.edu>
2009-05-20 06:52:40 +0000

    Moved version string back to "4.1.1 beta 1".  Had moved it to "4.1.1" too early.

--- global.h
+++ global.h
@@ -6,7 +6,7 @@
   29-Oct-03 Added version string and removed unused options from
 	    optstring.						TJB
 **********************************************************************/
-char *version = "4.1.1";
+char *version = "4.1.1 beta 1";
 
 char *optstring = "g:vo";
 

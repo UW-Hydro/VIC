1caf78dd85a7477609d47b264837f39a2cc869a5
vicadmin <vicadmin@hydro.washington.edu>
2012-02-09 02:47:06 +0000

    Miscellaneous typos.

--- initialize_model_state.c
+++ initialize_model_state.c
@@ -707,7 +707,7 @@ int initialize_model_state(dist_prcp_struct    *prcp,
 
           /* Check node spacing v time step */
           /* (note this is only approximate since heat capacity and conductivity can change considerably during the simulation depending on soil moisture and ice content) */
-          if (!options.IMPLICIT) {
+          if ((options.FROZEN_SOIL && !options.QUICK_FLUX) && !options.IMPLICIT) {
             dt_thresh = 0.5*energy[veg][band].Cs_node[1]/energy[veg][band].kappa_node[1]*pow((soil_con->dz_node[1]),2)/3600; // in hours
             if (global_param->dt > dt_thresh) {
               sprintf(ErrStr,"ERROR: You are currently running FROZEN SOIL with an explicit method (IMPLICIT is set to FALSE).  For the explicit method to be stable, time step %d hours is too large for the given thermal node spacing %f m, soil heat capacity %f J/m3/K, and soil thermal conductivity %f J/m/s/K.  Either set IMPLICIT to TRUE in your global parameter file (this is the recommended action), or decrease time step length to <= %f hours, or decrease the number of soil thermal nodes.",global_param->dt,soil_con->dz_node[1],energy[veg][band].Cs_node[1],energy[veg][band].kappa_node[1],dt_thresh);

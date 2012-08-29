1887a8fad51972bab67db3332d2fdeb95254acc8
vicadmin <vicadmin@hydro.washington.edu>
2012-08-29 00:21:00 +0000

    Updated for new release.

--- global.h
+++ global.h
@@ -20,7 +20,7 @@
 	      characteristics of bare soil.				TJB
   2012-Jan-16 Removed LINK_DEBUG code					BN
 **********************************************************************/
-char *version = "4.1.2.c bug fix update 2012-May-01";
+char *version = "4.1.2.d bug fix update 2012-Sep-01";
 
 char *optstring = "g:vo";
 

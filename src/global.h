bdb836e7c2020db6d054006e4730ca698568f9ca
Ted Bohn <ted.j.bohn@gmail.com>
2013-11-21 13:39:52 -0800

    Fixed two bugs: 1. Fixed incorrect starting forcing record when STARTHOUR != 0 2. Fixed incorrect disaggregation of moisture fluxes (e.g., precipitation) when ALMA_INPUT == TRUE

--- src/global.h
+++ src/global.h
@@ -20,7 +20,7 @@
 	      characteristics of bare soil.				TJB
   2012-Jan-16 Removed LINK_DEBUG code					BN
 **********************************************************************/
-char *version = "4.1.2.i bug fix update 2013-Sep-01";
+char *version = "4.1.2.j bug fix update 2013-Dec-01";
 
 char *optstring = "g:vo";
 

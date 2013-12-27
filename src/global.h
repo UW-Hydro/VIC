d23e6916acce14b716ff36ffd2917b9b2dfc0c32
Ted Bohn <ted.j.bohn@gmail.com>
2013-12-26 16:22:04 -0800

    Updated release string in global.h to reflect development branch.

--- src/global.h
+++ src/global.h
@@ -20,7 +20,7 @@
 	      characteristics of bare soil.				TJB
   2012-Jan-16 Removed LINK_DEBUG code					BN
 **********************************************************************/
-char *version = "4.1.2_wetcarb 2013-Jul-25";
+char *version = "4.2 beta 2013-Dec-26";
 
 char *optstring = "g:vo";
 

587fddd3a8bb76a1ef536bfd191884f633edc080
Ted Bohn <ted.j.bohn@gmail.com>
2014-04-02 11:06:01 -0700

    Fixed memory leak in vicNl.c.

--- src/vicNl.c
+++ src/vicNl.c
@@ -92,6 +92,8 @@ int main(int argc, char *argv[])
 	      read_soilparam().						TJB
   2012-Jan-16 Removed LINK_DEBUG code					BN
   2014-Mar-24 Removed ARC_SOIL option         BN
+  2014-Apr-02 Moved "free" statements for soil_con arrays outside the
+	      OUTPUT_FORCE condition to avoid memory leak.		TJB
 **********************************************************************/
 {
 
@@ -366,14 +368,14 @@ int main(int argc, char *argv[])
 #endif /* QUICK_FS */
       free_dist_prcp(&prcp,veg_con[0].vegetat_type_num);
       free_vegcon(&veg_con);
+      free((char*)init_STILL_STORM);
+      free((char*)init_DRY_TIME);
+#endif /* !OUTPUT_FORCE */
       free((char *)soil_con.AreaFract);
       free((char *)soil_con.BandElev);
       free((char *)soil_con.Tfactor);
       free((char *)soil_con.Pfactor);
       free((char *)soil_con.AboveTreeLine);
-      free((char*)init_STILL_STORM);
-      free((char*)init_DRY_TIME);
-#endif /* !OUTPUT_FORCE */
     }	/* End Run Model Condition */
   } 	/* End Grid Loop */
 

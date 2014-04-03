a925fe8e2b5c1a8879ef341baf5d2d08f6836794
Joe Hamman <jhamman@hydro.washington.edu>
2014-04-03 08:36:48 -0700

    update version string

--- src/global.h
+++ src/global.h
@@ -20,7 +20,7 @@
 	      characteristics of bare soil.				TJB
   2012-Jan-16 Removed LINK_DEBUG code					BN
 **********************************************************************/
-char *version = "4.1.2.l bug fix update 2014-Mar-01";
+char *version = "4.1.2.l bug fix update 2014-Apr-03";
 
 char *optstring = "g:vo";
 

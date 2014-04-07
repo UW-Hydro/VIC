31971fed409c2d5a1cf410ee5f76487df33f0d02
Bart Nijssen <nijssen@uw.edu>
2014-04-07 13:58:14 -0700

    Moved write_layer.c to vic_run
    Note that write_layer should NOT be part of vic_run. Instead, if
    data is to be written out it should be passed to the driver.

--- vic_run/include/vic_run.h
+++ vic_run/include/vic_run.h
@@ -299,5 +299,7 @@ int water_balance (lake_var_struct *, lake_con_struct, int, dist_prcp_struct *, 
 int water_energy_balance(int, double*, double*, int, int, double, double, double, double, double, double, double, double, double, double, double, double, double, double *, double *, double *, double*, double *, double *, double *, double, double *, double *, double *, double *, double *, double);
 int water_under_ice(int, double,  double, double *, double *, double, int, double, double, double, double *, double *, double *, double *, int, double, double, double, double *);
 void wrap_compute_zwt(soil_con_struct *, cell_data_struct *);
+void write_layer(layer_data_struct *, int, int, double *, double *);
+
 
 #endif

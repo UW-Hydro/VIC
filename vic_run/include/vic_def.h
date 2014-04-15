088fb51ea2942c507c82b1cc4907d9b2c4b6600d
Bart Nijssen <nijssen@uw.edu>
2014-04-14 17:31:34 -0700

    Moved force_type_struct and param_set_struct to vic_driver_classic.h

--- vic_run/include/vic_def.h
+++ vic_run/include/vic_def.h
@@ -944,30 +944,6 @@ typedef struct {
 } option_struct;
 
 /*******************************************************
-  Stores forcing file input information.
-*******************************************************/
-typedef struct {
-  char    SIGNED;
-  int     SUPPLIED;
-  double  multiplier;
-} force_type_struct;
-
-/******************************************************************
-  This structure records the parameters set by the forcing file
-  input routines.  Those filled, are used to estimate the paramters
-  needed for the model run in initialize_atmos.c.
-  ******************************************************************/
-typedef struct {
-  force_type_struct TYPE[N_FORCING_TYPES];
-  int  FORCE_DT[2];     /* forcing file time step */
-  int  FORCE_ENDIAN[2]; /* endian-ness of input file, used for
-			   DAILY_BINARY format */
-  int  FORCE_FORMAT[2]; /* ASCII or BINARY */
-  int  FORCE_INDEX[2][N_FORCING_TYPES];
-  int  N_TYPES[2];
-} param_set_struct;
-
-/*******************************************************
   This structure stores all model run global parameters.
   *******************************************************/
 typedef struct {

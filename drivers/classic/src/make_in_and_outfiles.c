ea23f0d1fceac89f78ac31c940b9821810236b30
Joe Hamman <jhamman@hydro.washington.edu>
2014-12-04 09:35:19 -0800

    add file suffixes .txt and .bin to ascii and binary outputs resepectively

--- drivers/classic/src/make_in_and_outfiles.c
+++ drivers/classic/src/make_in_and_outfiles.c
@@ -92,10 +92,12 @@ make_in_and_outfiles(filep_struct         *filep,
         strcat(out_data_files[filenum].filename, "_");
         strcat(out_data_files[filenum].filename, lngchar);
         if (options.BINARY_OUTPUT) {
+            strcat(out_data_files[filenum].filename, ".bin");
             out_data_files[filenum].fh = open_file(
                 out_data_files[filenum].filename, "wb");
         }
         else {
+            strcat(out_data_files[filenum].filename, ".txt");
             out_data_files[filenum].fh = open_file(
                 out_data_files[filenum].filename, "w");
         }

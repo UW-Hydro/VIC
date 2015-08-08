9806b593446c21cb5abf98186c7d3b681e2576b1
Joe Hamman <jhamman1@uw.edu>
2015-08-07 20:13:25 -0700

    remove unused functions in modify_Ksat.c, rename the source file to interpolation

--- tests/unit/vic_run/test_modify_Ksat.py
+++ tests/unit/vic_run/test_modify_Ksat.py
@@ -13,30 +13,3 @@ def test_linear_interp():
     for i in range(n - 1):
         assert vic_lib.linear_interp(new_x[i], x[i], x[i + 1],
                                      y[i], y[i + 1]) == scipy_new_y[i]
-
-
-# TODO: remove when modify_ksat is removed
-# def test_exp_interp():
-#     n = 10
-#     x = np.arange(n, dtype=np.float)
-#     y = np.arange(n, dtype=np.float)
-#     new_x = np.arange(n, dtype=np.float) + 0.5
-#     for i in range(n - 1):
-#         assert vic_lib.exp_interp(new_x[i], x[i], x[i + 1],
-#                                   y[i], y[i + 1]) > y[i]
-#         assert vic_lib.exp_interp(new_x[i], x[i], x[i + 1],
-#                                   y[i], y[i + 1]) < y[i + 1]
-# def test_modify_ksat():
-#     temps = np.linspace(-20, 20, num=50)
-#     for t in temps:
-#         assert vic_lib.modify_Ksat(t) == 1.
-
-
-# def test_modify_ksat_frozen():
-#     temps = np.linspace(-20, 20, num=50)
-#     vic_lib.options.FROZEN_SOIL = True
-#     for t in temps:
-#         factor = vic_lib.modify_Ksat(t)
-#         print(t, factor)
-#     assert factor > 1.
-#     assert factor <= 2.

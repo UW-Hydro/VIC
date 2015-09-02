5a646591204629ebf889ce48f545157ec21cb35b
Joe Hamman <jhamman1@uw.edu>
2015-09-02 14:11:20 -0700

    move out_data structures to shared driver

--- vic/drivers/shared/src/zero_output_list.c
+++ vic/drivers/shared/src/zero_output_list.c
@@ -24,8 +24,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *****************************************************************************/
 
-#include <vic_def.h>
-#include <vic_run.h>
+#include <vic_driver_shared.h>
 
 /******************************************************************************
  * @brief    This routine resets the values of all output variables to 0.

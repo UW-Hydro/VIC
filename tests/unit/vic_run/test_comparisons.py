e88f2329084a5aa680a8117c334294bedd5bbffc
Joe Hamman <jhamman1@uw.edu>
2015-09-15 12:59:01 -0700

    update comparisons module

--- tests/unit/vic_run/test_comparisons.py
+++ tests/unit/vic_run/test_comparisons.py
@@ -4,27 +4,7 @@ import numpy as np
 import pytest
 
 
-def test_assert_almost_equal():
-    xs = [1.0, 2.33333, 33, 77.7, 0.000001]
-    ys = [1.0, 2.33339, 34, 77.70000000001, -0.00001]
-
-    results = {}
-    results[3] = [True, True, False, True, True]
-    results[10] = [True, False, False, True, False]
-    results[12] = [True, False, False, False, False]
-
-    for decimal, rs in iteritems(results):
-        for x, y, r in pyzip(xs, ys, rs):
-            assert vic_lib.assert_almost_equal(x, y, decimal) == r
-            # make sure numpy agrees
-            if not r:
-                with pytest.raises(AssertionError):
-                    np.testing.assert_almost_equal(x, y, decimal=decimal)
-            else:
-                np.testing.assert_almost_equal(x, y, decimal=decimal)
-
-
-def test_assert_close():
+def test_assert_close_float():
     xs = [1e10, 1e-7, 1e10, 1e-8, 1e10, 1e-8, 1.]
     ys = [1.00001e10, 1e-8, 1.00001e10, 1e-9, 1.0001e10, 1e-9, 1.]
 
@@ -35,6 +15,21 @@ def test_assert_close():
     for tols, rs in iteritems(results):
         rtol, abs_tol = tols
         for x, y, r in pyzip(xs, ys, rs):
-            assert vic_lib.assert_close(x, y, rtol, abs_tol) == r
+            assert vic_lib.assert_close_float(x, y, rtol, abs_tol) == r
+            # make sure numpy agrees
+            assert np.isclose(x, y, rtol=rtol, atol=abs_tol) == r
+
+def test_assert_close_double():
+    xs = [1e10, 1e-7, 1e10, 1e-8, 1e10, 1e-8, 1.]
+    ys = [1.00001e10, 1e-8, 1.00001e10, 1e-9, 1.0001e10, 1e-9, 1.]
+
+    results = {}
+    results[(1e-05, 1e-08)] = [True, False, True, True, False, True, True]
+    results[(1e-03, 0)] = [True, False,  True, False,  True, False,  True]
+
+    for tols, rs in iteritems(results):
+        rtol, abs_tol = tols
+        for x, y, r in pyzip(xs, ys, rs):
+            assert vic_lib.assert_close_float(x, y, rtol, abs_tol) == r
             # make sure numpy agrees
             assert np.isclose(x, y, rtol=rtol, atol=abs_tol) == r
--- vic/vic_run/src/comparisons.c
+++ vic/vic_run/src/comparisons.c
@@ -29,14 +29,15 @@
 #include <vic_def.h>
 
 /******************************************************************************
- * @brief    returns false if two doubles are not equal up to desired precision
+ * @brief    returns false if two floats are not equal up to desired tolerance
  *****************************************************************************/
 bool
-assert_almost_equal(double x,
-                    double y,
-                    int    decimal)
+assert_close_float(float x,
+                   float y,
+                   float rtol,
+                   float abs_tol)
 {
-    if (fabs(x - y) < 0.5 * pow(10, -1 * decimal)) {
+    if (fabs(x - y) <= abs_tol + rtol * fabs(y)) {
         return true;
     }
     return false;
@@ -46,10 +47,10 @@ assert_almost_equal(double x,
  * @brief    returns false if two doubles are not equal up to desired tolerance
  *****************************************************************************/
 bool
-assert_close(double x,
-             double y,
-             double rtol,
-             double abs_tol)
+assert_close_double(double x,
+                    double y,
+                    double rtol,
+                    double abs_tol)
 {
     if (fabs(x - y) <= abs_tol + rtol * fabs(y)) {
         return true;

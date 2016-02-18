4eb8c9937ea46943242bf3f89b42ea4385ca8e15
Ted Bohn <ted.j.bohn@gmail.com>
2016-02-17 20:33:10 -0800

    Added error messages for use of deprecated VEGCOVER options.

--- vic/drivers/classic/src/get_global_param.c
+++ vic/drivers/classic/src/get_global_param.c
@@ -599,6 +599,11 @@ get_global_param(FILE *gp)
                     options.VEGLIB_FCAN = false;
                 }
             }
+            else if(strcasecmp("VEGLIB_VEGCOVER",optstr)==0) {
+                log_err("The option VEGLIB_VEGCOVER has been replaced by "
+                        "VEGLIB_FCAN.  Please edit your global parameter "
+                        "file and re-run.");
+            }
             else if (strcasecmp("VEGPARAM", optstr) == 0) {
                 sscanf(cmdstr, "%*s %s", filenames.veg);
             }
@@ -679,6 +684,16 @@ get_global_param(FILE *gp)
                     log_err("Unrecognized value of ALB_SRC in the global "
                             "control file.");
                 }
+            }
+            else if(strcasecmp("VEGPARAM_VEGCOVER",optstr)==0) {
+                log_err("The option VEGPARAM_VEGCOVER has been replaced by "
+                        "VEGPARAM_FCAN.  Please edit your global parameter "
+                        "file and re-run.");
+            }
+            else if(strcasecmp("VEGCOVER_SRC",optstr)==0) {
+                log_err("The option VEGCOVER_SRC has been replaced by "
+                        "FCAN_SRC.  Please edit your global parameter "
+                        "file and re-run.");
             }
             else if (strcasecmp("ROOT_ZONES", optstr) == 0) {
                 sscanf(cmdstr, "%*s %zu", &options.ROOT_ZONES);

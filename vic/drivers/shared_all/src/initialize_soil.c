c40be54812aaf2db66d237d29240f1f1e37c71e0
ymao <ymao@hydro.washington.edu>
2016-04-22 18:38:07 -0700

    Still initialize all variables to zero (not to empty)

--- vic/drivers/shared_all/src/initialize_soil.c
+++ vic/drivers/shared_all/src/initialize_soil.c
@@ -38,41 +38,40 @@ initialize_soil(cell_data_struct **cell,
     extern option_struct options;
 
     size_t               veg, band, lindex, frost_area;
-    double               dummy_double;
 
     for (veg = 0; veg <= veg_num; veg++) {
         for (band = 0; band < options.SNOW_BAND; band++) {
             // Prognostic states
-            cell[veg][band].aero_resist[0] = dummy_double;
-            cell[veg][band].aero_resist[1] = dummy_double;
-            cell[veg][band].CLitter = dummy_double;
-            cell[veg][band].CInter = dummy_double;
-            cell[veg][band].CSlow = dummy_double;
+            cell[veg][band].aero_resist[0] = 0.0;
+            cell[veg][band].aero_resist[1] = 0.0;
+            cell[veg][band].CLitter = 0.0;
+            cell[veg][band].CInter = 0.0;
+            cell[veg][band].CSlow = 0.0;
             for (lindex = 0; lindex < options.Nlayer; lindex++) {
-                cell[veg][band].layer[lindex].Cs = dummy_double;
-                cell[veg][band].layer[lindex].T = dummy_double;
-                cell[veg][band].layer[lindex].bare_evap_frac = dummy_double;
-                cell[veg][band].layer[lindex].evap = dummy_double;
+                cell[veg][band].layer[lindex].Cs = 0.0;
+                cell[veg][band].layer[lindex].T = 0.0;
+                cell[veg][band].layer[lindex].bare_evap_frac = 0.0;
+                cell[veg][band].layer[lindex].evap = 0.0;
                 for (frost_area = 0; frost_area < options.Nfrost;
                      frost_area++) {
-                    cell[veg][band].layer[lindex].ice[frost_area] = dummy_double;
+                    cell[veg][band].layer[lindex].ice[frost_area] = 0.0;
                 }
-                cell[veg][band].layer[lindex].kappa = dummy_double;
-                cell[veg][band].layer[lindex].moist = dummy_double;
-                cell[veg][band].layer[lindex].phi = dummy_double;
+                cell[veg][band].layer[lindex].kappa = 0.0;
+                cell[veg][band].layer[lindex].moist = 0.0;
+                cell[veg][band].layer[lindex].phi = 0.0;
             }
-            cell[veg][band].rootmoist = dummy_double;
-            cell[veg][band].wetness = dummy_double;
+            cell[veg][band].rootmoist = 0.0;
+            cell[veg][band].wetness = 0.0;
             // Fluxes
-            cell[veg][band].pot_evap = dummy_double;
-            cell[veg][band].baseflow = dummy_double;
-            cell[veg][band].runoff = dummy_double;
-            cell[veg][band].inflow = dummy_double;
-            cell[veg][band].RhLitter = dummy_double;
-            cell[veg][band].RhLitter2Atm = dummy_double;
-            cell[veg][band].RhInter = dummy_double;
-            cell[veg][band].RhSlow = dummy_double;
-            cell[veg][band].RhTot = dummy_double;
+            cell[veg][band].pot_evap = 0.0;
+            cell[veg][band].baseflow = 0.0;
+            cell[veg][band].runoff = 0.0;
+            cell[veg][band].inflow = 0.0;
+            cell[veg][band].RhLitter = 0.0;
+            cell[veg][band].RhLitter2Atm = 0.0;
+            cell[veg][band].RhInter = 0.0;
+            cell[veg][band].RhSlow = 0.0;
+            cell[veg][band].RhTot = 0.0;
         }
     }
 }
--- vic/drivers/shared_all/src/initialize_veg.c
+++ vic/drivers/shared_all/src/initialize_veg.c
@@ -36,48 +36,47 @@ initialize_veg(veg_var_struct **veg_var,
     extern option_struct options;
 
     size_t               i, j, k;
-    double               dummy_double;
 
     for (i = 0; i < Nveg; i++) {
         for (j = 0; j < options.SNOW_BAND; j++) {
             // Prognostic states
-            veg_var[i][j].albedo = dummy_double;
-            veg_var[i][j].displacement = dummy_double;
-            veg_var[i][j].fcanopy = dummy_double;
-            veg_var[i][j].LAI = dummy_double;
-            veg_var[i][j].roughness = dummy_double;
-            veg_var[i][j].Wdew = dummy_double;
-            veg_var[i][j].Wdmax = dummy_double;
+            veg_var[i][j].albedo = 0.0;
+            veg_var[i][j].displacement = 0.0;
+            veg_var[i][j].fcanopy = 0.0;
+            veg_var[i][j].LAI = 0.0;
+            veg_var[i][j].roughness = 0.0;
+            veg_var[i][j].Wdew = 0.0;
+            veg_var[i][j].Wdmax = 0.0;
             // Fluxes
-            veg_var[i][j].canopyevap = dummy_double;
-            veg_var[i][j].throughfall = dummy_double;
+            veg_var[i][j].canopyevap = 0.0;
+            veg_var[i][j].throughfall = 0.0;
         }
         if (options.CARBON) {
             for (j = 0; j < options.SNOW_BAND; j++) {
                 // Carbon states
-                veg_var[i][j].AnnualNPP = dummy_double;
-                veg_var[i][j].AnnualNPPPrev = dummy_double;
-                veg_var[i][j].Ci = dummy_double;
-                veg_var[i][j].NPPfactor = dummy_double;
-                veg_var[i][j].rc = dummy_double;
+                veg_var[i][j].AnnualNPP = 0.0;
+                veg_var[i][j].AnnualNPPPrev = 0.0;
+                veg_var[i][j].Ci = 0.0;
+                veg_var[i][j].NPPfactor = 0.0;
+                veg_var[i][j].rc = 0.0;
                 for (k = 0; k < options.Ncanopy; k++) {
-                    veg_var[i][j].CiLayer[k] = dummy_double;
-                    veg_var[i][j].NscaleFactor[k] = dummy_double;
-                    veg_var[i][j].rsLayer[k] = dummy_double;
+                    veg_var[i][j].CiLayer[k] = 0.0;
+                    veg_var[i][j].NscaleFactor[k] = 0.0;
+                    veg_var[i][j].rsLayer[k] = 0.0;
                 }
                 // Carbon fluxes
-                veg_var[i][j].aPAR = dummy_double;
+                veg_var[i][j].aPAR = 0.0;
                 for (k = 0; k < options.Ncanopy; k++) {
-                    veg_var[i][j].aPARLayer[k] = dummy_double;
+                    veg_var[i][j].aPARLayer[k] = 0.0;
                 }
-                veg_var[i][j].GPP = dummy_double;
-                veg_var[i][j].Litterfall = dummy_double;
-                veg_var[i][j].NPP = dummy_double;
-                veg_var[i][j].Raut = dummy_double;
-                veg_var[i][j].Rdark = dummy_double;
-                veg_var[i][j].Rgrowth = dummy_double;
-                veg_var[i][j].Rmaint = dummy_double;
-                veg_var[i][j].Rphoto = dummy_double;
+                veg_var[i][j].GPP = 0.0;
+                veg_var[i][j].Litterfall = 0.0;
+                veg_var[i][j].NPP = 0.0;
+                veg_var[i][j].Raut = 0.0;
+                veg_var[i][j].Rdark = 0.0;
+                veg_var[i][j].Rgrowth = 0.0;
+                veg_var[i][j].Rmaint = 0.0;
+                veg_var[i][j].Rphoto = 0.0;
             }
         }
     }

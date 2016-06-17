fdde7cf6c1a74ce78c41c42074c2b4909061647e
Joe Hamman <jhamman@hydro.washington.edu>
2016-06-16 17:15:29 -0700

    fix bug where global parameter file was closed twice

--- vic/drivers/classic/src/vic_classic.c
+++ vic/drivers/classic/src/vic_classic.c
@@ -117,7 +117,6 @@ main(int   argc,
     set_output_met_data_info();
     // out_data is shape [ngridcells (1), N_OUTVAR_TYPES]
     alloc_out_data(1, &out_data);
-    fclose(filep.globalparam);
     filep.globalparam = open_file(filenames.global, "r");
     parse_output_info(filep.globalparam, &streams, &(dmy[0]));
     validate_streams(&streams);

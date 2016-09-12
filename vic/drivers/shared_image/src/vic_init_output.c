8d64e34c08ead9a581b6063cae95bc0fd67d2152
yixinmao <yixinmao@uw.edu>
2016-09-12 16:02:30 -0700

    Fixed image driver history file name (#635)
    * Fixed image driver history file name timetamp - now is timstep-beginning
    
    * Added release notes about PR #635 (fixing image driver history filename
    timestamp)

--- vic/drivers/shared_image/src/vic_init_output.c
+++ vic/drivers/shared_image/src/vic_init_output.c
@@ -231,25 +231,29 @@ initialize_history_file(nc_file_struct *nc,
     case FREQ_NDAYS:
         sprintf(stream->filename, "%s/%s.%04d-%02d-%02d.nc",
                 filenames.result_dir,
-                stream->prefix, ref_dmy->year, ref_dmy->month,
-                ref_dmy->day);
+                stream->prefix, stream->time_bounds[0].year,
+                stream->time_bounds[0].month,
+                stream->time_bounds[0].day);
         break;
     case FREQ_NMONTHS:
         // If FREQ_NMONTHS -- filename = result_dir/prefix.YYYY-MM.nc
         sprintf(stream->filename, "%s/%s.%04d-%02d.nc", filenames.result_dir,
-                stream->prefix, ref_dmy->year, ref_dmy->month);
+                stream->prefix, stream->time_bounds[0].year,
+                stream->time_bounds[0].month);
         break;
     case FREQ_NYEARS:
         // If FREQ_NYEARS -- filename = result_dir/prefix.YYYY.nc
         sprintf(stream->filename, "%s/%s.%04d.nc", filenames.result_dir,
-                stream->prefix, ref_dmy->year);
+                stream->prefix, stream->time_bounds[0].year);
         break;
     default:
         // For all other cases -- filename = result_dir/prefix.YYYY-MM-DD-SSSSS.nc
         sprintf(stream->filename, "%s/%s.%04d-%02d-%02d-%05u.nc",
                 filenames.result_dir,
-                stream->prefix, ref_dmy->year, ref_dmy->month,
-                ref_dmy->day, ref_dmy->dayseconds);
+                stream->prefix, stream->time_bounds[0].year,
+                stream->time_bounds[0].month,
+                stream->time_bounds[0].day,
+                stream->time_bounds[0].dayseconds);
     }
 
     // open the netcdf file

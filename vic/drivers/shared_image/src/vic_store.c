95d69837d722e0267e2e92eb6764a7628be5f1fb
dgergel <dgergel@gmail.com>
2016-10-07 14:16:16 -0400

    use appropriate fillvalue datatypes in gather_put_nc_field fctns

--- vic/drivers/shared_image/src/vic_store.c
+++ vic/drivers/shared_image/src/vic_store.c
@@ -372,7 +372,7 @@ vic_store(dmy_struct *dmy_current,
             }
             gather_put_nc_field_int(nc_state_file.nc_id,
                                     nc_var->nc_varid,
-                                    nc_state_file.d_fillvalue,
+                                    nc_state_file.i_fillvalue,
                                     d4start, nc_var->nc_counts, ivar);
             for (i = 0; i < local_domain.ncells_active; i++) {
                 ivar[i] = nc_state_file.i_fillvalue;
@@ -835,7 +835,7 @@ vic_store(dmy_struct *dmy_current,
         }
         gather_put_nc_field_int(nc_state_file.nc_id,
                                 nc_var->nc_varid,
-                                nc_state_file.d_fillvalue,
+                                nc_state_file.i_fillvalue,
                                 d2start, nc_var->nc_counts, ivar);
         for (i = 0; i < local_domain.ncells_active; i++) {
             ivar[i] = nc_state_file.i_fillvalue;
@@ -848,7 +848,7 @@ vic_store(dmy_struct *dmy_current,
         }
         gather_put_nc_field_int(nc_state_file.nc_id,
                                 nc_var->nc_varid,
-                                nc_state_file.d_fillvalue,
+                                nc_state_file.i_fillvalue,
                                 d2start, nc_var->nc_counts, ivar);
         for (i = 0; i < local_domain.ncells_active; i++) {
             ivar[i] = nc_state_file.i_fillvalue;
@@ -994,7 +994,7 @@ vic_store(dmy_struct *dmy_current,
         }
         gather_put_nc_field_int(nc_state_file.nc_id,
                                 nc_var->nc_varid,
-                                nc_state_file.d_fillvalue,
+                                nc_state_file.i_fillvalue,
                                 d2start, nc_var->nc_counts, ivar);
         for (i = 0; i < local_domain.ncells_active; i++) {
             ivar[i] = nc_state_file.i_fillvalue;

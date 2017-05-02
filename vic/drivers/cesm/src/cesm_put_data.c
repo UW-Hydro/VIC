f46abf064a3b0afef88b4eafc10a326435e8a8e9
Diana Gergel <dgergel@gmail.com>
2017-05-02 14:59:19 -0700

    ran uncrustify

--- vic/drivers/cesm/src/cesm_put_data.c
+++ vic/drivers/cesm/src/cesm_put_data.c
@@ -166,7 +166,8 @@ vic_cesm_put_data()
 
         // lnd->rtm input fluxes
         l2x_vic[i].l2x_Flrl_rofliq = out_data[i][OUT_RUNOFF] +
-                                     out_data[i][OUT_BASEFLOW] / global_param.dt;
+                                     out_data[i][OUT_BASEFLOW] /
+                                     global_param.dt;
 
 
         // running sum to make sure we get the full grid cell

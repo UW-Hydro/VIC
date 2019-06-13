2f849edd67a2750766fc20bf3fc6a4be1b837cc0
Diana Gergel <dgergel@gmail.com>
2019-06-13 15:01:54 -0700

    fix travis tests again

--- tests/unit/vic_run/test_modify_Ksat.py
+++ tests/unit/vic_run/test_modify_Ksat.py
@@ -8,7 +8,7 @@ def test_linear_interp():
     x = np.arange(n, dtype=np.float)
     y = np.arange(n, dtype=np.float)
     new_x = np.arange(n, dtype=np.float) + 0.5
-    f = interp1d(x, y)
+    f = interp1d(x, y, bounds_error=False, fill_value='extrapolate')
     scipy_new_y = f(new_x)
     np.testing.assert_almost_equal(scipy_new_y[:5], [0.5, 1.5, 2.5, 3.5, 4.5])
     for i in range(n - 1):
